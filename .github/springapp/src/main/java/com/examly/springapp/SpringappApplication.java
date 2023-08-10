8f3fa7c565e4428bd996dd7e5dd7774afcd343ac
Iamneo <techadmin@iamneo.ai>
2023-08-10 06:04:12 +0000

    Backup Commit

--- .github/springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ .github/springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -8,6 +8,33 @@ public class SpringappApplication {
 
 	private static final String SpringappApp = null;
 
+	public class CalculateBMI {
+
+		//Enter the code here....
+		
+			public double calculate(double height, double weight) {
+				double b =(weight) / (height * height);
+				double d=Math.round(b*Math.pow(10, 2))/Math.pow(10,2);
+				return d;
+			}
+			
+			public String description(double B) {
+				if (B < 18.5) {
+					return "Under Weight";
+				} else if (B >= 18.5 && B< 24.9) {
+					return "Normal";
+				} else if (B >= 25 && B < 29.9) {
+					return "Over Weight";
+				} else if (B >= 30 && B < 34.9) {
+					return "Obese";
+				} else {
+					return "Extremely Obese";
+				}
+			}
+		
+		
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappApplication.class, args);
 		
